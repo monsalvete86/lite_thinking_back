8669d344233e41ef86f930a521d3ada026cbf314
cristhiam.monsalve@azuloapp.com <cristhiam.monsalve@azuloapp.com>
2023-01-05 17:10:21 -0500

    Add Acces control origin

--- app/routes/company.routes.js
+++ app/routes/company.routes.js
@@ -3,6 +3,7 @@ const { authJwt } = require("../middleware/index.js");
 
 module.exports = function(app) {
   app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
     res.header(
       "Access-Control-Allow-Headers",
       "x-access-token, Origin, Content-Type, Accept"
