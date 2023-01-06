c3949f8f4f00b39c10f214b975e718ad2da79ef4
cristhiam.monsalve@azuloapp.com <cristhiam.monsalve@azuloapp.com>
2023-01-05 21:45:11 -0500

    Add Acces control origin

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', '*')
+  res.header('Access-Control-Allow-Headers', '*')
+  next()
+})
+
 // parse requests of content-type - application/json
 app.use(express.json());
 
