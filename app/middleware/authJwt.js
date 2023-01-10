80ea6e37b2863f264657c29f3e5a4dc28c5ab23e
cristhiam.monsalve@azuloapp.com <cristhiam.monsalve@azuloapp.com>
2023-01-10 05:41:55 -0500

    change for barear toke n

--- app/middleware/authJwt.js
+++ app/middleware/authJwt.js
@@ -3,8 +3,8 @@ const config = require("../config/auth.config.js");
 const db = require("../models");
 const User = db.user;
 
-verifyToken = (req, res, next) => {
-  let token = req.headers["x-access-token"];
+const verifyToken = (req, res, next) => {
+  let token = req.headers["authorization"];
 
   if (!token) {
     return res.status(403).send({
@@ -12,7 +12,7 @@ verifyToken = (req, res, next) => {
     });
   }
 
-  jwt.verify(token, config.secret, (err, decoded) => {
+  jwt.verify(token.split(' ')[1], config.secret, (err, decoded) => {
     if (err) {
       return res.status(401).send({
         message: "Unauthorized!"
--- server.js
+++ server.js
@@ -12,9 +12,7 @@ app.use(cors(corsOptions));
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', '*')
-  res.header('Access-Control-Allow-Headers', '*')
-  res.header("Content-Type", "application/json");
-  res.header("Accept", "application/json");
+
   next()
 })
 
@@ -48,6 +46,7 @@ require('./app/routes/company.routes')(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
